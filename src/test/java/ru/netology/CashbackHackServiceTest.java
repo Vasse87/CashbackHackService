a535aa0c14327664de3ae4a84b0dd426f5da4388
Sonya test <0404pmm@mail.ru>
2023-05-26 11:48:09 +0300

    Add Tests with TestNG

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+    @Test
+    public void testRemainWhenPurchaseIsLessThanLowLimitForBonus() {
+        int expected = 100;
+        int actual = service.remain(900);
+        assertEquals (actual, expected);
+    }
+    @Test
+    public void testRemainWhenPurchaseIsZero() {
+        int expected = 1000;
+        int actual = service.remain(0);
+        assertEquals (actual, expected);
+    }
+
+    @Test
+    public void testRemainWhenPurchaseIsLessThanMinBoundaryForOne() {
+        int expected = 1;
+        int actual = service.remain(999);
+        assertEquals (actual, expected);
+    }
+
+    @Test
+    public void testRemainWhenPurchaseIsMoreThanMinBoundary() {
+        int expected = 999;
+        int actual = service.remain(1001);
+        assertEquals (actual, expected);
+    }
+
+    @Test
+    public void testRemainWhenPurchaseEqualsBoundary() {
+        int expected = 0;
+        int actual = service.remain(1000);
+        assertEquals (actual, expected);
+    }
+
+    @Test
+    public void testRemainWhenPurchaseMiltipleBoundary() {
+        int expected = 0;
+        int actual = service.remain(2000);
+        assertEquals (actual, expected);
+    }
+
+
+}
