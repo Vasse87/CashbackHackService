2a8cb8ae532b1eb74ef7e80e3b6f275463bb1647
Sonya test <0404pmm@mail.ru>
2023-05-26 12:02:31 +0300

    Add Tests with JUnit4

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,52 +1,53 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
+
     @Test
     public void testRemainWhenPurchaseIsLessThanLowLimitForBonus() {
         int expected = 100;
         int actual = service.remain(900);
-        assertEquals (actual, expected);
+        assertEquals(actual, expected);
     }
+
     @Test
     public void testRemainWhenPurchaseIsZero() {
         int expected = 1000;
         int actual = service.remain(0);
-        assertEquals (actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
     public void testRemainWhenPurchaseIsLessThanMinBoundaryForOne() {
         int expected = 1;
         int actual = service.remain(999);
-        assertEquals (actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
     public void testRemainWhenPurchaseIsMoreThanMinBoundary() {
         int expected = 999;
         int actual = service.remain(1001);
-        assertEquals (actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
     public void testRemainWhenPurchaseEqualsBoundary() {
         int expected = 0;
         int actual = service.remain(1000);
-        assertEquals (actual, expected);
+        assertEquals(actual, expected);
     }
 
     @Test
-    public void testRemainWhenPurchaseMiltipleBoundary() {
+    public void testRemainWhenPurchaseMultipleBoundary() {
         int expected = 0;
         int actual = service.remain(2000);
-        assertEquals (actual, expected);
+        assertEquals(actual, expected);
     }
-
-
 }
